be0fd1912d58751ee6bbe2ce6e69fd99b0452093
Aleksa Kovacevic <aleksakovacevicc@gmail.com>
2023-11-17 16:10:04 +0100

    Delete wrong filed

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recycleView: RecyclerView
+    private lateinit var foodList : ArrayList<Food>
+    private lateinit var foodAdapter : FoodAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycleView = findViewById(R.id.recyclerView)
+        recycleView.setHasFixedSize(true)
+        recycleView.layoutManager = LinearLayoutManager(this)
+
+        foodList = ArrayList()
+
+        foodList.add(Food(R.drawable.jabuka, "Jabuka"))
+        foodList.add(Food(R.drawable.banana, "Banana"))
+
+        foodAdapter = FoodAdapter(foodList)
+        recycleView.adapter = foodAdapter
+    }
+}
