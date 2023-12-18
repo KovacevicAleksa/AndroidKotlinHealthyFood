787e854fc3f11e0f3147c83d63370cf876e9801d
Aleksa Kovacevic <aleksakovacevicc@gmail.com>
2023-12-18 12:21:28 +0100

    Vracanje na grane bez api-a

--- app/src/main/java/com/example/myapplication/DetailedActivity.kt
+++ app/src/main/java/com/example/myapplication/DetailedActivity.kt
@@ -14,9 +14,11 @@ class DetailedActivity : AppCompatActivity() {
         val food = intent.getParcelableExtra<Food>("food")
         if (food != null ){
             val textView : TextView = findViewById(R.id.DetailedMain)
+            val textView1 : TextView = findViewById(R.id.DetailedTxt)
             val imageView : ImageView = findViewById(R.id.Detailedimg)
 
             textView.text = food.name
+            textView1.text = food.name
             imageView.setImageResource(food.image)
         }
     }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import android.util.Log
+import com.google.gson.JsonArray
+import org.json.JSONArray
+import java.io.IOException
+import java.io.InputStream
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +30,50 @@ class MainActivity : AppCompatActivity() {
 
         foodList = ArrayList()
 
+        val query = "1lb brisket and fries"
+        val apiKey = "ePjBXaX16JtfTmvEuNmgMA==ZOuYxeSINJjBeLXA"
+        val apiUrl = "https://api.api-ninjas.com/v1/nutrition?query=$query"
+
+        apiUrl
+            .httpGet()
+            .header("X-Api-Key" to apiKey)
+            .response { request, response, result ->
+                when (result) {
+                    is Result.Success -> {
+                        // Prikazi status koristeći response.status.value
+                        Log.d("Tag", "Status code: ${response.statusCode}")
+
+                        val data = String(response.data)
+                        var data1 = response.data
+
+                        // Prikazi telo odgovora kao string
+                        Log.d("Tag", "Response body: ${JSONArray(String(response.data)).getJSONObject(2)}")
+                        Log.d("Tag", "Response body data1: ${data1}")
+                        Log.d("Tag", "Response body data: ${data}")
+
+
+
+
+                    }
+                    is Result.Failure -> {
+                        val error = String(response.data)
+                        Log.d("Tag", "Error: $error")
+                    }
+                }
+            }
+
+
+
+
+
+
+
+        foodList.add(Food(R.drawable.jabuka, "Jabuka"))
+        foodList.add(Food(R.drawable.banana, "Banana"))
+        foodList.add(Food(R.drawable.limun,"Limun"))
+        foodList.add(Food(R.drawable.lubenica,"Lubenica"))
+        foodList.add(Food(R.drawable.jagoda,"Jagoda"))
+        foodList.add(Food(R.drawable.ananas,"Ananas"))
         foodList.add(Food(R.drawable.jabuka, "Jabuka"))
         foodList.add(Food(R.drawable.banana, "Banana"))
         foodList.add(Food(R.drawable.limun,"Limun"))
